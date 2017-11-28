59e2ef3ad2a24cf555e33b8489ed8788af60f289
Manoj <durvasula@yahoo.com>
2017-11-28 11:23:50 +0530

    Initial Commit

---
+++ script-includes/NewMatch5..js
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+var NewMatch5 = Class.create();
+gs.print("Test again");
+gs.print("Test test....");
+gs.print("BBSR");
+NewMatch5.prototype = {
+    initialize: function() {
+    },
+    testMethod : function(){
+		var gr = new GlideRecord('sys_user');
+		gr.addActiveQuery();
+		gr.query();
+		while(gr.next()){
+			var userId = gr.getValue('user_id');
+			var name = gr.getValue('name');
+			gs.print("UserDetails"+userId+":"+name);
+		}
+	},
+	testMethod1 : function(){
+		var inc = new GlideRecord('incident');
+       inc.addActiveQuery();
+       inc.query();
+		while(inc.next()){
+		var number = inc.getValue('number');
+		}
+	},
+    type: 'NewMatch5'
+};
+=======
+var NewMatch5 = Class.create();
+gs.print("Test again");
+gs.print("Test test....");
+gs.print("BBSR");
+NewMatch5.prototype = {
+    initialize: function() {
+    },
+    testMethod : function(){
+		var gr = new GlideRecord('sys_user');
+		gr.addActiveQuery();
+		gr.query();
+		while(gr.next()){
+			var userId = gr.getValue('user_id');
+			var name = gr.getValue('name');
+			gs.print("UserDetails"+userId+":"+name);
+		}
+	},
+	testMethod1 : function(){
+		var inc = new GlideRecord('incident');
+       inc.addActiveQuery();
+       inc.query();
+		while(inc.next()){
+		var number = inc.getValue('number');
+		}
+	},
+    type: 'NewMatch5'
+};
+>>>>>>> Merge
